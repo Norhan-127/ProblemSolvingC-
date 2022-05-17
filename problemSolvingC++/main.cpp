d03ee69ad4b7ff9b67eb793dc231851902e6868d
Norhan <norhanhamza127@gmail.com>
2022-05-17 12:03:10 +0200

    Initial Commit

---
+++ problemSolvingC++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  problemSolvingC++
+//
+//  Created by Nour on 17/05/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
